f98b5f321bbca6e2e6acaf0cb8d2de1a41edfbc9
collombet-23 <eliot.collombet@telecom-paris.fr>
2025-07-22 00:56:21 +0200

    Scroll accueil

---
+++ src/components/root/valeurs.tsx
@@ -0,0 +1,9 @@
+export default function Valeurs() {
+    return (
+        <section className="w-full h-[500px]" id="valeurs">
+            <p className="text-foreground-base title-small text-center">
+            Nos Valeurs
+            </p>
+        </section>
+    );
+}
