eb5a14c1d00949c0b167d0db4c1209c1179986fb
collombet-23 <eliot.collombet@gmail.com>
2025-08-01 03:40:13 +0200

    Meilleur test

--- src/components/analytics/google-analytics-provider.tsx
+++ src/components/analytics/google-analytics-provider.tsx
@@ -1,11 +1,15 @@
 "use client";
 import GoogleAnalytics from "@/components/analytics/google-analytics";
 import useKlaroConsent from "@/components/cookies/use-klaro-consent";
+import { useEffect } from "react";
 
 export default function GoogleAnalyticsProvider() {
     const hasConsent = useKlaroConsent("google-analytics");
 
-    console.log(hasConsent)
+    useEffect(() => {
+        console.log(hasConsent)
+    }, [hasConsent]);
+
     return hasConsent ? (
         <GoogleAnalytics/>
     ) : null;
