7bf1c5dc68048b531d9c9ac46f8d40d82a3cca54
collombet-23 <eliot.collombet@gmail.com>
2025-08-01 04:25:11 +0200

    oublié l'update

--- public/klaro-config.js
+++ public/klaro-config.js
@@ -56,6 +56,7 @@ var klaroConfig = {
             required: false,
             default: false,
             onlyOnce: true,
+            callback: function(consent, app) { notifyConsentChange(app.name, consent); }
         },
     ],
 }
