cff553ba6b4cf5c711ac1df96b961e42a3cf0a4b
collombet-23 <eliot.collombet@gmail.com>
2025-08-01 04:15:46 +0200

    log

--- public/klaro-config.js
+++ public/klaro-config.js
@@ -34,6 +34,7 @@ var klaroConfig = {
         }
     },
     callback: function(consent, app, opts) { // Refresh si google-analytics a changé
+        console.log(consents, app, opts);
         if (app && app.name === "google-analytics" && opts && opts.changed) {
             setTimeout(() => window.location.reload(), 100);
         }
