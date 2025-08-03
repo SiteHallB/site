60533e67347bb4ff6eb33ef5d9c9bb8e98925480
collombet-23 <eliot.collombet@gmail.com>
2025-08-03 21:57:09 +0200

    merci

--- src/app/merci/page.tsx
+++ src/app/merci/page.tsx
@@ -4,7 +4,7 @@ export default function ThankYouPage() {
     return (
         <div className="w-full h-[100vh] bg-background-base flexCenter flex-col">
             <h1 className="text-foreground-base mb-2">Merci !</h1>
-            <p className="text-foreground-subdued mb-4">Vous vous êtes inscrit avec succès, à très vite</p>
+            <p className="text-foreground-subdued mb-4">Vous vous êtes inscrit avec succès, à très vite. Vérifiez votre boite mail et rendez-vous sur l'application Sportigo.</p>
             <Clickable
                 clickableType={{type: "link", path: "/"}}
                 style={{variant: "action", color: "primary"}}
