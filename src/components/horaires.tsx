3d44b760af05bc28140115b25ece3396143d0604
SiteHallBAdmin <site.hallb@gmail.com>
2025-08-19 22:17:11 +0200

    point horaires

--- src/components/horaires.tsx
+++ src/components/horaires.tsx
@@ -5,7 +5,7 @@ export default function Horaires() {
             <p className="text-foreground-base textSubH2 mb-1 lg:mb-contentClose">Horaires</p>
             <p className="text-foreground-subdued">
                 - Ouverture 6h-23h 7/7j<br/>
-                - <span className="text-accent">Réception</span> : LUN-VEN 9h-13h.16h-20h / SAM 9h-13h
+                - <span className="text-accent">Réception</span> : LUN-VEN 9h-13h • 16h-20h / SAM 9h-13h
             </p>
             </div>
         </div>
