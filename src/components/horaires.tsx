cbfcf2657d12452eeaca886879433651b57ff017
SiteHallBAdmin <site.hallb@gmail.com>
2025-09-06 13:47:32 +0200

    Horaires réception en jaiune

--- src/components/horaires.tsx
+++ src/components/horaires.tsx
@@ -4,8 +4,8 @@ export default function Horaires() {
             <div>
             <p className="text-foreground-base textSubH2 mb-1 lg:mb-contentClose">Horaires</p>
             <p className="text-foreground-subdued">
-                - Ouverture 6h-23h 7/7j<br/>
-                - <span className="text-accent">Réception</span> : LUN-VEN 9h-13h • 16h-20h / SAM 9h-13h
+                Ouvert 6h-23h 7/7j<br/>
+                <span className="text-accent">Horaires de réception LUN-VEN 9h-13h • 16h-20h / SAM 9h-13h</span>
             </p>
             </div>
         </div>
