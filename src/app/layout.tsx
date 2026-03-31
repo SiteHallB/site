879876d0461083c2c3268186f1f593647544e890
SiteHallBAdmin <site.hallb@gmail.com>
2026-03-31 12:39:28 +0200

    feat(pop-up): suppression du pop-up jusqu'à nouvel ordre

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -135,7 +135,7 @@ export default function RootLayout({
                         <Menu/>
                     </header>
 
-                    <PopUp/>
+                    {/* <PopUp/> */}
                     <Toast/>
 
                     {/* Contenu principal */}
