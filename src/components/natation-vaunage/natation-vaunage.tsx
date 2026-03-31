3b1d355632ee2d2a7d3c0efe9c7b5fdee0a7b7ac
SiteHallBAdmin <site.hallb@gmail.com>
2026-03-31 12:51:29 +0200

    feat(natation enfant): meilleur alignement sur téléphone et suppression texte superflue

--- src/components/natation-vaunage/natation-vaunage.tsx
+++ src/components/natation-vaunage/natation-vaunage.tsx
@@ -2,7 +2,7 @@
 
 import PageStructure from "@/components/ui/page-structure";
 
-import { Sun, Baby, WavesLadder, CheckCheck, CalendarCheck2 } from "lucide-react";
+import { Baby, WavesLadder, CheckCheck, CalendarCheck2 } from "lucide-react";
 import { PhoneNumber } from "@/components/ui/contact-widget";
 import Clickable from "@/components/ui/clickable";
 import { useImage } from "@/context/image-context";
@@ -14,10 +14,6 @@ export default function NatationVaunage() {
     return (
         <PageStructure title="Natation Enfant" subtitle={<p>Cours de natation en vaunage - Piscine intérieure à Saint Dionisy.</p>}>
             <div className="bg-background-highlight rounded-xl w-full p-4 flex-col">
-                <div className="flex flex-row items-center justify-start">
-                    <Sun className="mr-2 text-accent size-5 lg:size-7 shrink-0"/>
-                    <p>L'été approche ! Pensez à la sécurité aquatique de vos enfants tout en leur faisant découvrir le plaisir de l'eau.</p>
-                </div>
                 <p className="my-2">Au Hall B à Saint-Dionisy, nous proposons :</p>
                 <div className="flex flex-row items-center justify-start">
                     <Baby className="mr-2 text-accent size-5 lg:size-7 shrink-0"/>
@@ -31,12 +27,14 @@ export default function NatationVaunage() {
                     <CheckCheck className="mr-2 text-accent size-5 lg:size-7 shrink-0"/>
                     <p><em>Tests d'aisance aquatique certifiants</em> - indispensables pour certaines activités nautiques et colonies</p>
                 </div>
-
-                <p className="my-2">Encadrés par des maitres nageurs diplomés et avec la garantie d'avoir maximum 5 enfants par cours, les enfants apprennent à être en sécurité dans l'eau tout en s'amusant.</p>
-
-                <div className="flex flex-wrap items-center justify-start">
+            
+                <div className="flex flex-row">
                     <CalendarCheck2 className="mr-2 text-accent size-5 lg:size-7 shrink-0"/>
-                    <p>Paiement à la séance : réservation par téléphone (</p><PhoneNumber/><p className="mr-2">) ou en achetant directement votre séance/carnet de séances en ligne</p>
+                    <p>Paiement à la séance : réservation par téléphone ou en achetant directement votre séance/carnet de séances en ligne</p>
+                </div>
+
+                <div className="my-8 flex flex-row h-full justify-center space-x-4">
+                    <PhoneNumber/>
                     <Clickable
                         clickableType={{type: "link", path: "/tarifs/carnet-seances"}}
                         style={{}}
